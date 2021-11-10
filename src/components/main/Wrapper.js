24bd47c21e4d1ac38edd74833d68d32521056da4
Emmanueldmlr <bakaredmlr@gmail.com>
2021-11-10 18:05:22 +0100

    feature: implements pokeman logic

---
+++ src/components/main/Wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Wrapper = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+
--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,26 +1,16 @@
-import React,{useState, useEffect}  from 'react'
+import React, { useState, useEffect } from 'react'
 
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import { getPokemon } from '../services/pokemonService';
 
-interface gameInterface{
+import Loader from "react-loader-spinner";
 
-    weight: number,
-
-    moves: { move : {name: string, url: string}, version_group_details: any}[],
-
-    types: {slot: number, type: {name: string, url: string}}[],
-
-    stats: {base_stat: number, effort: number, stat: {name: string, url: string}}[],
-
-    species: {name: string, url: string}
-
-}
+import gameInterface from '../interface/gameInterface'
 
 const Pokemon: React.FC = () => {
 
-    const {name}: {name:string} = useParams()
+    const { name }: { name: string } = useParams()
 
     const [loading, setIsLoading] = useState<boolean>(true)
 
@@ -30,35 +20,42 @@ const Pokemon: React.FC = () => {
 
     const retrievePokemon = async (gameName: string) => {
 
-        const result = await getPokemon(gameName)
+        try {
 
-        const { data }: { data: gameInterface  } = result
+            const result = await getPokemon(gameName)
 
-        setGame(data)
+            const { data }: { data: gameInterface } = result
 
-        setTimeout(() => {
+            setGame(data)
+
+            setTimeout(() => {
+
+                setIsLoading(false)
+
+            }, 1000)
+
+        }
+        catch (err) {
 
             setIsLoading(false)
 
-        }, 1000)
+            setError("Not Found")
+
+        }
+
 
     }
 
     useEffect(() => {
 
-        if(!game){
+        if (!game) {
 
-            try{
+            retrievePokemon(name)
 
-                retrievePokemon(name)
+        }
+        else {
 
-            }
-
-            catch(err){
-
-                console.log(err)
-
-            }
+            setIsLoading(false)
 
         }
 
@@ -74,147 +71,190 @@ const Pokemon: React.FC = () => {
 
                     <div className="row">
 
-                        <div className="col-lg-8" id="mainContent">
+                        {
 
-                            <div className="row">
+                            loading ?
 
-                                <div className="col-lg-6" id="sidebar">
+                                <div className="offset-md-5 offset-4" >
 
-                                    <h5>Species</h5>
+                                    <Loader
 
-                                    <div id="faqNavContainer" className="theiaStickySidebar">
+                                        type="Puff"
 
-                                        <ul id="faqNav">
+                                        color="#ff7b79"
 
-                                            <li><a href="#" className="active nice-scroll-faq">Name: bulbasaur</a></li>
+                                        height={60}
 
-                                            <li><a href="#" className="active nice-scroll-faq">URL: https://pokeapi.co/api/v2/pokemon-species/1/</a></li>
+                                        width={60}
 
-                                        </ul>
-                                    </div>
+                                    />
+
                                 </div>
 
-                                <div className="col-lg-6" id="sidebar">
+                                :
+                                
+                                game ?
 
-                                    <h5>Weight</h5>
+                                    <>
 
-                                    <div id="faqNavContainer" className="theiaStickySidebar">
+                                        <div className="col-lg-8" id="mainContent">
 
-                                        <ul id="faqNav">
+                                            <div className="row">
 
-                                            <li><a href="#" className="active nice-scroll-faq">60KG</a></li>
+                                                <div className="col-lg-6" id="sidebar">
 
-                                        </ul>
-                                    </div>
-                                </div>
+                                                    <h5>Species</h5>
 
-                                <div className="col-lg-6" id="sidebar">
+                                                    <div id="faqNavContainer" className="theiaStickySidebar">
 
-                                    <h5>Stats</h5>
+                                                        <ul id="faqNav">
 
-                                    <div id="orderContainer" className="theiaStickySidebar">
-                                        <div className="row">
-                                            <div className="col-md-12">
+                                                            <li><a href="#" className="active nice-scroll-faq">Name: {game.species.name}</a></li>
 
-                                                <div className="row total-container">
-                                                    <div className="col-8 p-0">
-                                                        <h5>Name</h5>
+                                                            <li><a href="#" className="active nice-scroll-faq">URL: {game.species.url}</a></li>
+
+                                                        </ul>
                                                     </div>
+                                                </div>
 
-                                                    <div className="col-4 p-0">
-                                                        <h5>Base Stats</h5>
-                                                    </div>
+                                                <div className="col-lg-6" id="sidebar">
 
-                                                    <div className="col-8 p-0">
-                                                        <span>Defence</span>
-                                                    </div>
+                                                    <h5>Weight</h5>
 
-                                                    <div className="col-4 p-0">
-                                                        <span>60</span>
-                                                    </div>
-                                                    <div className="col-8 p-0">
-                                                        <span>Attack</span>
-                                                    </div>
+                                                    <div id="faqNavContainer" className="theiaStickySidebar">
 
-                                                    <div className="col-4 p-0">
-                                                        <span>200</span>
+                                                        <ul id="faqNav">
+
+                                                            <li><a href="#" className="active nice-scroll-faq">{game.weight}</a></li>
+
+                                                        </ul>
                                                     </div>
+                                                </div>
+
+                                                <div className="col-lg-6" id="sidebar">
+
+                                                    <h5>Stats</h5>
+
+                                                    <div id="orderContainer" className="theiaStickySidebar">
+                                                        <div className="row">
+                                                            <div className="col-md-12">
+
+                                                                <div className="row total-container">
+                                                                    <div className="col-8 p-0">
+                                                                        <h5>Name</h5>
+                                                                    </div>
+
+                                                                    <div className="col-4 p-0">
+                                                                        <h5>Base Stats</h5>
+                                                                    </div>
+
+                                                                    {
+
+                                                                        game.stats.map((stat, index) => (
+
+                                                                            <>
+                                                                                <div className="col-8 p-0">
+                                                                                    <span>{stat.stat.name}</span>
+                                                                                </div>
+
+                                                                                <div className="col-4 p-0">
+                                                                                    <span>{stat.base_stat}</span>
+                                                                                </div>
+                                                                            </>
+                                                                        ))
+
+                                                                    }
+
+
+                                                                </div>
+                                                                <div id="totalError"></div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-lg-6" id="sidebar">
+
+                                                    <h5>Types</h5>
+
+                                                    <div id="orderContainer" className="theiaStickySidebar">
+                                                        <div className="row">
+                                                            <div className="col-md-12">
+
+                                                                <div className="row total-container">
+                                                                    <div className="col-8 p-0">
+                                                                        <h5>Name</h5>
+                                                                    </div>
+
+                                                                    <div className="col-4 p-0">
+                                                                        <h5>Slot</h5>
+                                                                    </div>
+
+                                                                    {
+                                                                        game.types.map((type) => (
+
+                                                                            <>
+                                                                                <div className="col-8 p-0">
+                                                                                    <span>{type.type.name}</span>
+                                                                                </div>
+
+                                                                                <div className="col-4 p-0">
+                                                                                    <span>{type.slot}</span>
+                                                                                </div>
+                                                                            </>
+                                                                        ))
+                                                                    }
+
+
+                                                                </div>
+                                                                <div id="totalError"></div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12" id="sidebar">
+
+                                                <h5>Moves</h5>
+
+                                                <div id="orderContainer" className="theiaStickySidebar">
+
+                                                    {
+                                                        game.moves.map((move, index) => (
+
+                                                            <span className="terms-link" key={index}>{move.move.name}</span>
+
+                                                        ))
+                                                    }
 
                                                 </div>
-                                                <div id="totalError"></div>
+
                                             </div>
+
                                         </div>
-                                    
-                                    </div>
-                                </div>
 
-                                <div className="col-lg-6" id="sidebar">
+                                        <div className="col-lg-4" id="sidebar">
 
-                                    <h5>Types</h5>
+                                            <div id="orderContainer" className="theiaStickySidebar">
 
-                                    <div id="orderContainer" className="theiaStickySidebar">
-                                        <div className="row">
-                                            <div className="col-md-12">
+                                                <div className="box text-center">
 
-                                                <div className="row total-container">
-                                                    <div className="col-8 p-0">
-                                                        <h5>Name</h5>
-                                                    </div>
-
-                                                    <div className="col-4 p-0">
-                                                        <h5>Slot</h5>
-                                                    </div>
-
-                                                    <div className="col-8 p-0">
-                                                        <span>grass</span>
-                                                    </div>
-
-                                                    <div className="col-4 p-0">
-                                                        <span>1</span>
-                                                    </div>
-                                                    <div className="col-8 p-0">
-                                                        <span>poison</span>
-                                                    </div>
-
-                                                    <div className="col-4 p-0">
-                                                        <span>2</span>
-                                                    </div>
+                                                    <img alt="game" src="/rubik.png" height="200" width="200" />
 
                                                 </div>
-                                                <div id="totalError"></div>
+
                                             </div>
                                         </div>
-                                    
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-12" id="sidebar">
 
-                                    <h5>Moves</h5>
+                                    </>
+                                    :
 
-                                    <div id="orderContainer" className="theiaStickySidebar">
-                                        <span className="terms-link">bind</span>
-                                        <span className="terms-link">vine-whip</span>
-                                        <span className="terms-link">headbutt</span>
-                                        <span className="terms-link">tackle</span>
-                                    </div>
-                                    
-                            </div>
+                                    <h4 className="offset-md-5 offset-4 text-danger">Not Found</h4>
 
-                        </div>
+                        }
 
-                        <div className="col-lg-4" id="sidebar">
-
-                            <div id="orderContainer" className="theiaStickySidebar">
-
-                                <div className="box text-center">
-
-                                    <img alt="game" src="/rubik.png" height="200" width="200" />
-
-                                </div>
-
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
